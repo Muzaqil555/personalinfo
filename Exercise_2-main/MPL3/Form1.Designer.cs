f00935a1406a5de4d290807e762254d4abe69b06
Muzaqil555 <mhesenli7@std.beu.edu.az>
2025-09-28 23:33:15 +0400

    Add files via upload

---
+++ Exercise_2-main/MPL3/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MPL3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            comboBox1 = new ComboBox();
+            listBox1 = new ListBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            btnShowInfo = new Button();
+            btnclose = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(listBox1);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(75, 43);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(456, 315);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Personal Information";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Azerbaijan", "Russia", "Turkey" });
+            comboBox1.Location = new Point(297, 29);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(138, 28);
+            comboBox1.TabIndex = 8;
+            comboBox1.Text = "Country";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "Baku", "Ganja", "Lankaran", "Shaki" });
+            listBox1.Location = new Point(143, 29);
+            listBox1.Margin = new Padding(3, 4, 3, 4);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(137, 184);
+            listBox1.TabIndex = 7;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox4.Location = new Point(344, 252);
+            checkBox4.Margin = new Padding(3, 4, 3, 4);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(82, 29);
+            checkBox4.TabIndex = 6;
+            checkBox4.Text = "other";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox3.Location = new Point(229, 252);
+            checkBox3.Margin = new Padding(3, 4, 3, 4);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(72, 29);
+            checkBox3.TabIndex = 5;
+            checkBox3.Text = "Java";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox2.Location = new Point(127, 252);
+            checkBox2.Margin = new Padding(3, 4, 3, 4);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(72, 29);
+            checkBox2.TabIndex = 4;
+            checkBox2.Text = "C++";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(25, 252);
+            checkBox1.Margin = new Padding(3, 4, 3, 4);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(57, 29);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "C#";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton3.Location = new Point(23, 168);
+            radioButton3.Margin = new Padding(3, 4, 3, 4);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(90, 29);
+            radioButton3.TabIndex = 2;
+            radioButton3.Text = "Other..";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(23, 104);
+            radioButton2.Margin = new Padding(3, 4, 3, 4);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(95, 29);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(23, 47);
+            radioButton1.Margin = new Padding(3, 4, 3, 4);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(76, 29);
+            radioButton1.TabIndex = 0;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(353, 371);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(94, 29);
+            btnShowInfo.TabIndex = 1;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // btnclose
+            // 
+            btnclose.Location = new Point(467, 371);
+            btnclose.Name = "btnclose";
+            btnclose.Size = new Size(94, 29);
+            btnclose.TabIndex = 2;
+            btnclose.Text = "Close";
+            btnclose.UseVisualStyleBackColor = true;
+            btnclose.Click += btnclose_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 64, 0);
+            ClientSize = new Size(587, 412);
+            Controls.Add(btnclose);
+            Controls.Add(btnShowInfo);
+            Controls.Add(groupBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Box";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox4;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Button btnShowInfo;
+        private Button btnclose;
+    }
+}
---
+++ Exercise_2-main/MPL3/Form1.cs
@@ -0,0 +1,42 @@
+namespace MPL3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            string gender = "";
+            if (radioButton1.Checked)
+                gender = "Male";
+            else if (radioButton2.Checked)
+                gender = "Female";
+            else if (radioButton3.Checked)
+                gender = "Other";
+
+            string city = listBox1.SelectedItem != null ? listBox1.SelectedItem.ToString() : "Not selected";
+            string country = comboBox1.SelectedItem != null ? comboBox1.SelectedItem.ToString() : "Not selected";
+
+            string languages = "";
+            if (checkBox1.Checked) languages += "C#, ";
+            if (checkBox2.Checked) languages += "C++, ";
+            if (checkBox3.Checked) languages += "Java, ";
+            if (checkBox3.Checked) languages += "Other, ";
+
+            if (languages.EndsWith(", "))
+                languages = languages.Substring(0, languages.Length - 2);
+
+            string result = $"Gender: {gender}\nCity: {city}\nCountry: {country}\nLanguages: {languages}";
+
+            MessageBox.Show(result, "User Information");
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
